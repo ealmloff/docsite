b6981e0d5454d1c7d669e4d5a948210d61e4bc65
Demonthos <evanalmloff@gmail.com>
2023-07-26 23:23:33 +0000

    Deploying to docs from @ Demonthos/docsite@910728989e1f605ab1fdacd0d782750dfeeb3c62 🚀

--- docs/assets/dioxus/snippets/dioxus-interpreter-js-7853214f69bd9fe6/inline0.js
+++ docs/assets/dioxus/snippets/dioxus-interpreter-js-7853214f69bd9fe6/inline0.js
@@ -163,7 +163,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
       function truthy(val) {
         return val === "true" || val === true;
       }
-    let s = "";let lsp,sp,sl; let c = new TextDecoder();let u8buf,u8bufp;let u32buf,u32bufp;const evt = [];
+    let s = "";let lsp,sp,sl; let c = new TextDecoder();let u32buf,u32bufp;let u8buf,u8bufp;const evt = [];
                     let evt_tmp1, evt_tmp2;
                     function get_evt() {
                         evt_tmp2 = u8buf[u8bufp++];
@@ -200,7 +200,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                             return ns_cache[ns_cache_tmp2&4294967167];
                         }
                     }
-            let ns,id,field,value,bubbles,event_name,len,ptr;
+            let ns,len,ptr,event_name,bubbles,field,value,id;
             export function create(r){
                 d=r;
             }
@@ -239,13 +239,13 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                     s = c.decode(new DataView(m.buffer, lsp, sl));
                 }
             }
-            sp=0;if ((metaflags>>>5)&1){
-                u8buf=new Uint8Array(m.buffer,m.getUint32(d+5*4,true))
-            }
-            u8bufp=0;if ((metaflags>>>3)&1){
+            sp=0;if ((metaflags>>>3)&1){
                 u32buf=new Uint32Array(m.buffer,m.getUint32(d+3*4,true))
             }
-            u32bufp=0;
+            u32bufp=0;if ((metaflags>>>5)&1){
+                u8buf=new Uint8Array(m.buffer,m.getUint32(d+5*4,true))
+            }
+            u8bufp=0;
                 for(;;){
                     op=m.getUint32(p,true);
                     p+=4;
